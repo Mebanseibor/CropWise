190579ca59efa09fa04738551a14a2a729d40c82
Mebanseibor Lyngdoh <mebanseiborlm@gmail.com>
2025-03-01 22:09:28 +0530

    Implemented a timed splash screen to transition into the main activity - Slightly chaned the size of the text in the splash screen

--- app/src/main/java/com/example/cropwise/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cropwise/SplashScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cropwise
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +12,11 @@ class SplashScreenActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
