e7f07b3b2125e5304aad60b3432d76c3fa9f7668
Mebanseibor Lyngdoh <mebanseiborlm@gmail.com>
2025-03-02 14:11:42 +0530

    Updated the splash screen - Added an emblem - Animated the emblem - Added a progress bar to indicate loading

--- app/src/main/java/com/example/cropwise/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cropwise/SplashScreenActivity.kt
@@ -4,19 +4,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity(){
+    private lateinit var cropwiseEmblem : ImageView
+
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splashscreen)
 
+        initFindViews()
+        initAnimation()
+
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 4000)
+    }
+
+    private fun initFindViews(){
+        cropwiseEmblem = findViewById(R.id.cropwise_emblem)
+    }
+
+    private fun initAnimation(){
+        fun animateEmblem() {
+            val animZoom = AnimationUtils.loadAnimation(this, R.anim.zoom_centertocenter)
+            cropwiseEmblem.startAnimation(animZoom)
+        }
+        animateEmblem()
     }
 }
