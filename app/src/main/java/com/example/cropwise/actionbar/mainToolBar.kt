9d8858ce741dd4d49e06ae505d2ec2d682b3133c
Mebanseibor <mebanseiborlm@gmail.com>
2025-03-19 13:51:25 +0530

    Added the gitignore folder - Replaced the first parameter of the mainToolbar object from an   activity to a context

--- app/src/main/java/com/example/cropwise/actionbar/mainToolBar.kt
+++ app/src/main/java/com/example/cropwise/actionbar/mainToolBar.kt
@@ -1,6 +1,6 @@
 package com.example.cropwise.actionbar
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
@@ -10,19 +10,16 @@ import com.example.cropwise.fragment.MainMenu_Category
 import com.example.cropwise.mainFragmentContainer
 import com.example.cropwise.notification.NotificationActivity
 import com.example.cropwise.toast.displayAboutApp
-import com.example.cropwise.toast.displayComingSoon
 
-class mainToolBar(activity : Activity , mainActionBar : Toolbar){
+class mainToolBar(private val context : Context , mainActionBar : Toolbar){
     lateinit var mainActionBar : Toolbar
     var menu : Int = R.menu.main_toolbar
-    lateinit var activity : Activity
 
     init {
         this.mainActionBar = mainActionBar
-        this.activity = activity
 
         mainActionBar.setOnLongClickListener{
-            displayAboutApp(activity.baseContext)
+            displayAboutApp(context)
 
             true
         }
@@ -39,15 +36,13 @@ class mainToolBar(activity : Activity , mainActionBar : Toolbar){
                 true
             }
             R.id.menu_item_settings -> {
-
                 val intent = Intent(context, SettingsActivity::class.java)
                 context.startActivity(intent)
-//                displayComingSoon(context, "Settings")
                 true
             }
             R.id.menu_item_notification -> {
-                val intent = Intent(activity.baseContext, NotificationActivity::class.java)
-                activity.startActivity(intent)
+                val intent = Intent(context, NotificationActivity::class.java)
+                context.startActivity(intent)
                 true
             }
             else -> {true}
