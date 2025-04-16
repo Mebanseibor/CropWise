87b64dcf9e0c6c3fa6b1a6ced0e6be9a50c64397
Mebanseibor <mebanseiborlm@gmail.com>
2025-04-16 10:03:09 +0530

    Added shared preferences to save the rating of the app

--- app/src/main/java/com/example/cropwise/rating/AppRating.kt
+++ app/src/main/java/com/example/cropwise/rating/AppRating.kt
@@ -1,6 +1,8 @@
 package com.example.cropwise.rating
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.RatingBar
 import android.widget.Toast
@@ -12,13 +14,38 @@ class AppRating : AppCompatActivity(){
     private lateinit var ratingBar : RatingBar
     private lateinit var btnSubmitRating : Button
 
+    // savedPreference
+    private var SP_name: String = "appRating"
+    private lateinit var sharedPref : SharedPreferences
+    private var keyAppRating : String = "appRating"
+    private var SP_appRating : Float = 0.0.toFloat()
+
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apprating)
 
+        initFetchSharedPreferences()
         initViews()
 
         initRatingBar()
+    }
+
+    private fun initFetchSharedPreferences(){
+        sharedPref = getSharedPreferences(SP_name, MODE_PRIVATE)
+
+
+        val value_appRating = sharedPref.getString(keyAppRating, "0.0")
+
+        if(value_appRating == null){
+            Log.d("App Rating", "value_appRating was null")
+            SP_appRating = 0.0.toFloat()
+            return
+        }
+
+        Log.d("App Rating", "value_appRating was NOT null")
+        SP_appRating = value_appRating.toFloat()
+
+        Log.d("App Rating", "SP_appRating = ${value_appRating.toFloat()}")
     }
 
     private fun initViews(){
@@ -27,14 +54,20 @@ class AppRating : AppCompatActivity(){
     }
 
     private fun initRatingBar(){
-        ratingBar.rating = 0.0.toFloat()
+        ratingBar.rating = SP_appRating
 
         ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
             Toast.makeText(this, "New rating changed to ${ratingBar.rating}", Toast.LENGTH_SHORT).show()
         }
 
         btnSubmitRating.setOnClickListener{
+            val editor = sharedPref.edit()
+
+            editor.putString(keyAppRating, ratingBar.rating.toString())
+            editor.apply()
+
             Toast.makeText(this, "Thanks for rating us ${ratingBar.rating}", Toast.LENGTH_SHORT).show()
+
             finish()
         }
     }
